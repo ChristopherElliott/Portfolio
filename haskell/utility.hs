52d8a02a7bba80c25b15ed34381f60201b285863
Christopher Elliott <mail@christopher-elliott.com>
2018-05-19 10:03:21 -0700

    Initial languages

---
+++ haskell/utility.hs
@@ -0,0 +1,11 @@
+data Expr = Val Int | Div 
+
+eval :: Expr -> Int 
+eval (Val n) = n 
+eval (Div x y) = eval x / eval y
+
+safeDiv :: Int -> Int -> Maybe Int 
+safediv n m = if m == 0 then 
+                 Nothing 
+                else 
+                    Just (n / m)
