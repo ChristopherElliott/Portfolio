ac12efbe24c6656c1e62bb2390a270dea6493dad
Christopher Elliott <mail@christopher-elliott.com>
2019-01-01 07:28:20 -0800

    Updates

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -9,14 +9,3 @@ someFunc = do
             putStrLn "someFunc"
             
 
-data Expr = Val Int | Div 
-
-eval :: Expr -> Int 
-eval (Val n) = n 
-eval (Div x y) = eval x / eval y
-
-safediv :: Int -> Int -> Maybe Int 
-safediv n m = if m == 0 then 
-                 Nothing 
-                else 
-                    Just (n / m)
--- haskell/src/utility.hs
+++ haskell/src/utility.hs
@@ -2,7 +2,7 @@ data Expr = Val Int | Div
 
 eval :: Expr -> Int 
 eval (Val n) = n 
-eval (Div x y) = eval x / eval y
+eval (`Div` x y) = eval x / eval y
 
 safediv :: Int -> Int -> Maybe Int 
 safediv n m = if m == 0 then 
